72096bd0881083a7f002db2aeafd6b512b90d6fe
Parysnm <parys.noyon@gmail.com>
2025-01-23 17:58:22 +0100

    Fix test to match updated response text

--- td5/scripts/sample-app/app.test.js
+++ td5/scripts/sample-app/app.test.js
@@ -2,18 +2,21 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Test the app', () => {
+  // Test de la route "/"
   test('Get / should return DevOps Labs!', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('DevOps Labs!');
   });
 
-  test('Get /name/Bob should return DevOps Labs!', async () => {
+  // Test de la route "/name/Bob"
+  test('Get /name/Bob should return Hello, Bob!', async () => {
     const response = await request(app).get('/name/Bob');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('DevOps Labs!');
+    expect(response.text).toBe('Hello, Bob!');
   });
 
+  // Test de la désinfection des entrées
   const maliciousUrl = '/name/%3Cscript%3Ealert("hi")%3C%2Fscript%3E';
   const sanitizedHtml = 'Hello, &lt;script&gt;alert(&#34;hi&#34;)&lt;/script&gt;!';
 
@@ -23,4 +26,5 @@ describe('Test the app', () => {
     expect(response.text).toBe(sanitizedHtml);
   });
 });
+
 
