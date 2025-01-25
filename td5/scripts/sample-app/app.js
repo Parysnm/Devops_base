4158c967107d964de8a25f3362787f6ea9d289b2
Parysnm <parys.noyon@gmail.com>
2025-01-25 10:59:41 +0100

    Mise à jour de .DS_Store et app.js

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const http = require('http');
 
-const app = express();
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.send('DevOps Labs!');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('DevOps Labs!');
 });
 
-app.get('/name/:name', (req, res) => {
-  res.render('Devops Labs!', {name: req.params.name});
+const port = process.env.PORT || 8080;
+server.listen(port,() => {
+  console.log(`Listening on port ${port}`);
 });
-
-module.exports = app;
